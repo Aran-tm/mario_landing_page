ec612252bdec87d160aa288721bd0e853a36ae7c
Aran-tm <armandortmontes@gmail.com>
2024-02-28 10:04:56 -0500

    adding tailwind css locally

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+ content: [
+    './index.html',
+    './styles.css'
+ ],
+ // Aquí puedes agregar otras configuraciones si las necesitas
+ theme: {
+    extend: {},
+ },
+ plugins: [],
+}
